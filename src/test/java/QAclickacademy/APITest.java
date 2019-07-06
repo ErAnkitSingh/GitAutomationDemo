22ea3ff9c451287c6a0cf978f1123fde79627b8d
ErAnkitSingh <ankitsumit98@gmail.com>
2019-07-06 11:10:47 +0530

    MyFirstCommit

---
+++ src/test/java/QAclickacademy/APITest.java
@@ -0,0 +1,19 @@
+package QAclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void Demo(){
+		System.out.println("Hey Successfully run testng.xml through POM.xml");
+
+	}
+
+	@Test
+	public void SecondCase(){
+		System.out.println("Hello from POM to Testng");
+	}
+
+
+}
---
+++ src/test/java/QAclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package QAclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void Demo(){
+		System.out.println("Hey TC1");
+
+	}
+
+	@Test
+	public void SecondCase(){
+		System.out.println("Hello TC2");
+	}
+
+}
